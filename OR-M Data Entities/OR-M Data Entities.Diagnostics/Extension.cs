6c4d7d362259870130d968481509ecb39fad9ddf
James Demeuse <james.demeuse@gmail.com>
2015-10-28 10:04:48 -0500

    10/28/2015

---
+++ OR-M Data Entities/OR-M Data Entities.Diagnostics/Extension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using OR_M_Data_Entities.Expressions;
+using OR_M_Data_Entities.Expressions.Resolution;
+using OR_M_Data_Entities.Mapping;
+
+namespace OR_M_Data_Entities.Diagnostics
+{
+    public static class Extension
+    {
+        public static string GetSystemTableSqlString(this Type type)
+        {
+            return string.Format("{0}Tables", _getSystemSqlString(type));
+        }
+
+        public static string GetSystemColumnsSqlString(this Type type)
+        {
+            return string.Format("{0}Columns", _getSystemSqlString(type));
+        }
+
+        private static string _getSystemSqlString(this Type type)
+        {
+            var linkedServerAttribute = type.GetCustomAttribute<LinkedServerAttribute>();
+
+            return linkedServerAttribute == null
+                ? "Sys."
+                : string.Format("[{0}].[{1}].Sys.", linkedServerAttribute.ServerName,
+                    linkedServerAttribute.DatabaseName);
+        }
+
+        public static string GetSql<T>(this ExpressionQuery<T> query)
+        {
+            var resolvableQuery = (IExpressionQueryResolvable)query;
+
+            // execute query
+            resolvableQuery.ResolveExpression();
+
+            return resolvableQuery.Sql;
+        }
+    }
+}
---
+++ OR-M Data Entities/OR-M Data Entities.Diagnostics/HealthMonitoring/Health.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace OR_M_Data_Entities.Diagnostics.HealthMonitoring
+{
+    public class Health
+    {
+        public Health()
+        {
+            _fails = new List<HealthError>();
+            _passes = new List<HealthCheck>();
+        }
+
+        public bool IsHealthy
+        {
+            get { return _fails.Count == 0; }
+        }
+
+        public decimal Percentage
+        {
+            get
+            {
+                return _passes.Count == 0 && _fails.Count == 0 || _passes.Count > 0 && _fails.Count == 0
+                    ? 100
+                    : ((_fails.Count/(_fails.Count + _passes.Count))*100);
+            }
+        }
+
+        public IEnumerable<HealthError> Fails {
+            get { return _fails; }
+        }
+        public List<HealthError> _fails { get; set; }
+
+        public IEnumerable<HealthCheck> Passes {
+            get { return _passes; }
+        }
+        public List<HealthCheck> _passes { get; set; }
+
+        public void Add(HealthError error)
+        {
+            _fails.Add(error);
+        }
+
+        public void Add(HealthCheck check)
+        {
+            _passes.Add(check);
+        }
+    }
+
+    public class HealthCheck
+    {
+        public HealthCheck(Check check)
+        {
+            Check = check;
+        }
+
+        public readonly Check Check;
+    }
+
+    public class HealthError : HealthCheck
+    {
+        public readonly string Remediation;
+
+        public HealthError(Check check, string remediation)
+            : base(check)
+        {
+            Remediation = remediation;
+        }
+    }
+}
