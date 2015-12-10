c39179ab4ee476d6439c7541a76cda02cf0e658e
James Demeuse <james.demeuse@gmail.com>
2015-12-10 10:34:40 -0600

    missed files

---
+++ OR-M Data Entities/OR-M Data Entities/Data/Definition/DeleteEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using OR_M_Data_Entities.Data.Modification;
+
+namespace OR_M_Data_Entities.Data.Definition
+{
+    public class DeleteEntity : ModificationEntity
+    {
+        public DeleteEntity(object entity) 
+            : base(entity, true)
+        {
+            ModificationItems = GetColumns().Select(w => new ModificationItem(w)).ToList();
+        }
+    }
+}
